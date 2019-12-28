1ca046c2025225782dffb43b75788a055b9d6bee
rifkyhilman <rifkyhilman265@gmail.com>
2019-12-28 23:03:34 +0700

    firts commit

---
+++ src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+  import Hello from "./components/hello.svelte";
+  export let name;
+  export let age = "12";
+  var gbr =
+    "https://images.unsplash.com/photo-1504639725590-34d0984388bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80";
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  <Hello buah="apel" sayuran="tomat" />
+  <h1>Hello {name}!</h1>
+  <p>
+    {age}Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+  <img src={gbr} alt="gambar" />
+</main>
---
+++ src/components/hello.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let buah;
+  export let sayuran;
+  let nama = "hello<b>dunia</b>";
+  let angka = 0;
+
+  function tambahsatu() {
+    angka = angka + 1;
+  }
+</script>
+
+<p>
+  {@html nama}
+</p>
+
+<h1>{angka} {buah} {sayuran}</h1>
+<button on:click={tambahsatu}>click</button>
